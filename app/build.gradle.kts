6ff79535794178b25c885dca6e63b3ebd27496fd
Nomvuselelo Akhona Mlambo <ST10264503@vcconnect.edu.za>
2025-05-01 21:33:47 +0200

    add stats page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,10 @@ dependencies {
     implementation ("androidx.room:room-runtime:2.7.1")
     annotationProcessor ("androidx.room:room-compiler:2.7.1")
 
-    implementation ("androidx.room:room-ktx:2.7.1")
+    implementation ("androidx.room:room-ktx:2.7.1 ")
     kapt ("androidx.room:room-compiler:2.7.1")
+
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
+
+
 }
