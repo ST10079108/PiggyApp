f7fb1eedd0018e0e20c378f2a71d918c16d5f817
gatekeeper321 <131513420+gatekeeper321@users.noreply.github.com>
2025-05-02 10:31:20 +0200

    transaction update

--- app/src/main/java/com/fake/piggyapp/transactionActivity.kt
+++ app/src/main/java/com/fake/piggyapp/transactionActivity.kt
@@ -1,20 +1,37 @@
 package com.fake.piggyapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.fake.piggyapp.database.AppDatabase
+import com.fake.piggyapp.database.UserDAO
+import com.fake.piggyapp.databinding.ActivityTransactionBinding
 
 class transactionActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityTransactionBinding
+    private lateinit var userDAO: UserDAO
+    private lateinit var db: AppDatabase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_transaction)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // View Binding
+        binding = ActivityTransactionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Initialize DB and DAO
+        db = AppDatabase.getDatabase(this) as AppDatabase
+        userDAO = db.userDAO()
+
+        binding.btnAddTransactionPage.setOnClickListener {
+            val intent = Intent(this, AddTransactionActivity::class.java)
+            startActivity(intent)
         }
+
     }
 }
